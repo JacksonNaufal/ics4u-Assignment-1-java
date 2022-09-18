6741fd1db4856b71b024f7ab855a56e2d35136e1
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-18 16:22:22 +0000

    fixing

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -36,25 +36,25 @@ final class RockPaperScissors {
     * @param args No args will be used
     */
     public static void main(final String[] args) {
-        // identifying the strings
-        // rock, paper and scissors
+        // identifying the strings.
+        // rock, paper and scissors.
         final String rock = "rock";
         final String paper = "paper";
         final String scissors = "scissors";
         final String botplayer = "(botPlayer)";
 
-        // max number in the Math.Random generator
+        // max number in the Math.Random generator.
         final int max = 3;
         final Scanner in = new Scanner(System.in);
         System.out.print("Enter your choice, Rock, Paper, or Scissors: ");
         final String userInput = in.nextLine();
 
-        // try and catch for invalid inputs
+        // try and catch for invalid inputs.
         if (!userInput.equals(rock) && !userInput.equals(paper)
                     && !userInput.equals(scissors)) {
             System.out.println("Invalid Input");
         } else {
-            // random bot choice
+            // random bot choice.
             final int randomNumber = (int) (Math.random() * max);
             String botPlayer = "";
             if (randomNumber == 0) {
@@ -66,7 +66,7 @@ final class RockPaperScissors {
             }
 
             // program decides if bot, or user wins, using standared
-            // Rock, Paper, Scissors rules
+            // Rock, Paper, Scissors rules.
             if (userInput.equals(botPlayer)) {
                 System.out.println("It was a Tie!");
                 System.out.println("The bot also played: " + botPlayer);
